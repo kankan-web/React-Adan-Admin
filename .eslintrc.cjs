ffafb94b4d87b0133f092e244ced6943eef5d556
kankan <2728360489@qq.com>
2024-03-18 00:06:24 +0800

    build: 搭建项目规范
    添加eslint、prettier、husky和commitizen规范

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,53 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+	"env": {
+		"browser": true,
+		"es2021": true,
+		"node": true
+	},
+	"settings": {
+		"react": {
+			"createClass": "createReactClass", // Regex for Component Factory to use,
+			// default to "createReactClass"
+			"pragma": "React",  // Pragma to use, default to "React"
+			"fragment": "Fragment",  // Fragment to use (may be a property of <pragma>), default to "Fragment"
+			"version": "detect", // React version. "detect" automatically picks the version you have installed.
+			// You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+			// It will default to "latest" and warn if missing, and to "detect" in the future
+			"flowVersion": "0.53" // Flow version
+		},
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:react/recommended",
+		"prettier",
+		"plugin:react/jsx-runtime",
+		"plugin:prettier/recommended"
+	],
+	"overrides": [
+		{
+			"env": {
+				"node": true
+			},
+			"files": [
+				".eslintrc.{js,cjs}"
+			],
+			"parserOptions": {
+				"sourceType": "script"
+			}
+		}
+	],
+	"parser": "@typescript-eslint/parser",
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"plugins": [
+		"@typescript-eslint",
+		"react",
+		"prettier"
+	],
+	"rules": {
+
+	}
 }
